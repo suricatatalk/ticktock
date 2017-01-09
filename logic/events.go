6cfb116fc3cbaca9f370f35b96a33707be2e4077
Radek Sohlich <sohlich@gmail.com>
2017-01-09 19:11:42 +0100

    another task cannot start if there is running one

--- logic/events.go
+++ logic/events.go
@@ -1,9 +1,13 @@
 package logic
 
-import "github.com/sohlich/ticktock/security"
-import "github.com/sohlich/ticktock/domain"
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/sohlich/ticktock/domain"
+	"github.com/sohlich/ticktock/security"
+)
 
 type EventDTO struct {
 	TaskName        string `bson:"taskName" json:"taskName"`
@@ -16,10 +20,14 @@ type EventFunction func(user security.User, event *EventDTO) (*domain.Task, erro
 
 func Start(user security.User, event *EventDTO) (*domain.Task, error) {
 	tasks, err := domain.Tasks.FindAllByStatusAndOwner("running", user.ID)
-	for _, val := range tasks {
-		val.ChangeState(domain.Finish)
-		domain.Tasks.Save(val)
+	if len(tasks) > 0 {
+		return nil, fmt.Errorf("Another task is running")
 	}
+
+	// for _, val := range tasks {
+	// 	val.ChangeState(domain.Finish)
+	// 	domain.Tasks.Save(val)
+	// }
 
 	task := domain.NewTask()
 	task.Status = domain.StatusRunning
@@ -32,15 +40,21 @@ func Start(user security.User, event *EventDTO) (*domain.Task, error) {
 }
 
 func Pause(user security.User, event *EventDTO) (*domain.Task, error) {
+	logChange("Pause", user.ID, event.TaskID)
 	return changeState(domain.Pause, user, event)
 }
 
 func Resume(user security.User, event *EventDTO) (*domain.Task, error) {
+	logChange("Resume", user.ID, event.TaskID)
 	return changeState(domain.Start, user, event)
 }
 
 func Finish(user security.User, event *EventDTO) (*domain.Task, error) {
+	logChange("Finish", user.ID, event.TaskID)
 	return changeState(domain.Finish, user, event)
+}
+func logChange(event, userID, taskID string) {
+	log.Printf("[%s] %s task_id: %s ", userID, event, taskID)
 }
 
 func changeState(action int, user security.User, event *EventDTO) (*domain.Task, error) {
@@ -52,6 +66,12 @@ func changeState(action int, user security.User, event *EventDTO) (*domain.Task,
 		return nil, fmt.Errorf("User is not owner of task")
 	}
 	task.ChangeState(action)
+
+	duration := int64(0)
+	for _, tsk := range task.Events {
+		duration = duration + tsk.Duration
+	}
+	task.Duration = duration
 	domain.Tasks.Save(task)
 	return task, nil
 }
